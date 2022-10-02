a3fadffb62ed07932adfe8f9254c63896d409a30
fireman <fireman2747@yandex.ru>
2022-10-02 23:14:44 +0800

    add Homework 002 квадрат числа

---
+++ Homework002/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Write("Введите номер дня недели: ");
+int i = int.Parse(Console.ReadLine());
+
+if (i == 1) 
+{
+    Console.WriteLine("Понедельник");
+}
+
+if (i == 2) 
+{
+    Console.WriteLine("Вторник");
+}
+
+if (i == 3) 
+{
+    Console.WriteLine("Среда");
+}
+
+if (i == 4) 
+{
+    Console.WriteLine("Четверг");
+}
+
+if (i == 5) 
+{
+    Console.WriteLine("Пятница");
+}
+
+if (i == 6) 
+{
+    Console.WriteLine("Суббота");
+}
+
+if (i == 7) 
+{
+    Console.WriteLine("Воскресенье");
+}
+
+if (i >= 8)
+{
+    Console.WriteLine("Извините, вы ввели не правильный номер!");
+    
+}
+ 
