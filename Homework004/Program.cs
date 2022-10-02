2c519046887f4efd9f2e45f406c67c24d4252539
fireman <fireman2747@yandex.ru>
2022-10-03 00:40:40 +0800

    add Homwork 004 even & not even

---
+++ Homework004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if ((a % 2) == 0)
+{
+    Console.Write("Число четное");
+}
+else 
+{
+     Console.Write("Число не четное");
+}
+
