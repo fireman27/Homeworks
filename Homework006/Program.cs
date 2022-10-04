edb0c6fd815a9560f4a4276bf63ac3dc2fa7d3c9
fireman <fireman2747@yandex.ru>
2022-10-04 10:29:07 +0800

    add Homework 006 last number

--- Homework006/Program.cs
+++ Homework006/Program.cs
@@ -2,14 +2,6 @@
 Console.WriteLine("Введите трехзначное число");
 int N = int.Parse(Console.ReadLine() ?? "0");
 
-while(N >= 100)
-{
-N = N - 100;
-} 
-while(N >= 10)
-{
-N = N - 10;
-}
+System.Console.WriteLine($"Второй цифрой числа {N} является {N / 1 % 10} ");
 
-Console.Write("Последняя цифра трехзначного числа ");
-Console.WriteLine(N);
+System.Console.WriteLine(); 
