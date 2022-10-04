b5337c029b7305e1fcc43b1331a4a0477be2343e
fireman <fireman2747@yandex.ru>
2022-10-04 10:27:30 +0800

    add Homework 007 Two number

---
+++ Homework007/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+System.Console.WriteLine($"Второй цифрой числа {N} является {N / 10 % 10} ");
+
+System.Console.WriteLine(); 
