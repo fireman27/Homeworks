0aafe9f0682e5ca512b571308482e1bdb99963f4
fireman <fireman2747@yandex.ru>
2022-10-04 22:59:21 +0800

    add Homework 011

---
+++ Homework011/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+if (N > 100 && N < 999)
+{
+    int a = (N % 10);
+    Console.Write("Третья цифра заданного числа: ");
+    Console.WriteLine (a);
+}
+
+if (N > 1000 && N< 10000)
+{
+    int a = ((N / 10) % 10);
+    Console.Write("Третья цифра заданного числа:  ");
+    Console.WriteLine (a);
+}
+else 
+{
+    Console.WriteLine("Введите другое число");
+}
+
+if (N < 100)
+{
+    Console.Write("Число ");
+    Console.Write(N); 
+    Console.WriteLine(" не имеет третьей цифры");
+}
