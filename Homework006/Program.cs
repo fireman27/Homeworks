3f3391b565e7c6951f475519838a9192a2f2e690
fireman <fireman2747@yandex.ru>
2022-10-04 13:01:04 +0800

    add Homework 006 fixed a bug

--- Homework006/Program.cs
+++ Homework006/Program.cs
@@ -2,6 +2,6 @@
 Console.WriteLine("Введите трехзначное число");
 int N = int.Parse(Console.ReadLine() ?? "0");
 
-System.Console.WriteLine($"Второй цифрой числа {N} является {N / 1 % 10} ");
+System.Console.WriteLine($"Третьей цифрой числа {N} является {N / 1 % 10} ");
 
 System.Console.WriteLine(); 
