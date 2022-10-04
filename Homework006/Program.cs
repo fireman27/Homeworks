cb1055dc681ff62fac0945d5277c05db8434c428
fireman <fireman2747@yandex.ru>
2022-10-04 10:15:13 +0800

    add Homework 00 last number

---
+++ Homework006/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+while(N >= 100)
+{
+N = N - 100;
+} 
+while(N >= 10)
+{
+N = N - 10;
+}
+
+Console.Write("Последняя цифра трехзначного числа ");
+Console.WriteLine(N);
