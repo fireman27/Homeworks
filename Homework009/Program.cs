3c3fd695377a688004c4d01d7a11cbc3277f5535
fireman <fireman2747@yandex.ru>
2022-10-04 10:58:07 +0800

    add Homework 009 delete second nomber

---
+++ Homework009/Program.cs
@@ -0,0 +1,14 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int a = 0;
+
+int NumberWithoutSecDig()
+{
+    a = N / 100 * 10 + N % 10;
+    return a;
+}
+
+NumberWithoutSecDig();
+
+System.Console.WriteLine($"Если из числа {N} удалить вторую цифру, получится число {a}");
