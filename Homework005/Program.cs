214802261516a5ee9c95015dcf72db3937b993d7
fireman <fireman2747@yandex.ru>
2022-10-04 09:38:46 +0800

    add Homework 005 even numbers

---
+++ Homework005/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Здесь появятся четные числа до N. Введите значение конца диапазона N");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int i = 2;
+while(i <= N)
+{
+    Console.WriteLine(i);
+    i = i + 2;
+} 
