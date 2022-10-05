858ae0d08e8dff15da098a42ce6831f87828a7a0
fireman <fireman2747@yandex.ru>
2022-10-05 08:46:04 +0800

    add Homework 002 описание

--- Homework002/Program.cs
+++ Homework002/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите номер дня недели: ");
+﻿// По заданному номеру дня недели вывести его название
+Console.Write("Введите номер дня недели: ");
 int i = int.Parse(Console.ReadLine() ?? "0"); // исправил ошибку (желтый текст)
 
 if (i == 1) 
@@ -6,37 +7,37 @@ if (i == 1)
     Console.WriteLine("Понедельник");
 }
 
-if (i == 2) 
+else if (i == 2) 
 {
     Console.WriteLine("Вторник");
 }
 
-if (i == 3) 
+else if (i == 3) 
 {
     Console.WriteLine("Среда");
 }
 
-if (i == 4) 
+else if (i == 4) 
 {
     Console.WriteLine("Четверг");
 }
 
-if (i == 5) 
+else if (i == 5) 
 {
     Console.WriteLine("Пятница");
 }
 
-if (i == 6) 
+else if (i == 6) 
 {
     Console.WriteLine("Суббота");
 }
 
-if (i == 7) 
+else if (i == 7) 
 {
     Console.WriteLine("Воскресенье");
 }
 
-if (i >= 8)
+else if (i >= 8)
 {
     Console.WriteLine("Извините, вы ввели не правильный номер!");
     
