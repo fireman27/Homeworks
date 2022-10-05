ffff850493199e7fd174749fb64a83edeb89ae5f
fireman <fireman2747@yandex.ru>
2022-10-05 09:17:43 +0800

    add Homework 007 поправил решение

--- Homework007/Program.cs
+++ Homework007/Program.cs
@@ -2,6 +2,16 @@
 Console.WriteLine("Введите трехзначное число");
 int N = int.Parse(Console.ReadLine() ?? "0");
 
-System.Console.WriteLine($"Второй цифрой числа {N} является {N / 10 % 10} ");
-
-System.Console.WriteLine(); 
+if (N <= 100) 
+{
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}
+else if (N >= 1000) 
+{
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}
+else
+{
+    System.Console.WriteLine($"Второй цифрой числа {N} является {N / 10 % 10} ");
+    System.Console.WriteLine(); 
+}
