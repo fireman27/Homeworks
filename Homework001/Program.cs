aab2ce71b4ab072cd775e7fc3f0adfb41d9da88e
fireman <fireman2747@yandex.ru>
2022-10-05 08:42:53 +0800

    add Homework 001 описание

--- Homework001/Program.cs
+++ Homework001/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите первое число: ");
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+Console.Write("Введите первое число: ");
 int a = int.Parse(Console.ReadLine() ?? "0"); // опять желтый текст
 Console.Write("Введите второе число: ");
 int b = int.Parse(Console.ReadLine() ?? "0");
