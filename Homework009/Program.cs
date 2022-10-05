385910b90f810340a47b2ca78fe9ecbd94d8fc4c
fireman <fireman2747@yandex.ru>
2022-10-05 09:24:34 +0800

    add Homework 009 поправил решение

--- Homework009/Program.cs
+++ Homework009/Program.cs
@@ -3,12 +3,21 @@ Console.WriteLine("Введите трехзначное число");
 int N = int.Parse(Console.ReadLine() ?? "0");
 int a = 0;
 
-int NumberWithoutSecDig()
+if (N <= 100) 
 {
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}
+else if (N >= 1000) 
+{
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}
+else
+{
+    int NumberWithoutSecDig()
+    {
     a = N / 100 * 10 + N % 10;
     return a;
+    }
+    NumberWithoutSecDig();
+    System.Console.WriteLine($"Если из числа {N} удалить вторую цифру, получится число {a}");
 }
-
-NumberWithoutSecDig();
-
-System.Console.WriteLine($"Если из числа {N} удалить вторую цифру, получится число {a}");
