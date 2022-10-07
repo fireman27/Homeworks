0b31b134d0820d3dbeac5d0e97b1fca86f1c098f
fireman <fireman2747@yandex.ru>
2022-10-07 22:50:13 +0800

    Homework 012 выходной?

---
+++ Homework012/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.Write("Введите номер дня недели: ");
+int daynumber = int.Parse(Console.ReadLine() ?? "0");
+if (daynumber > 5 && daynumber < 8)
+    {
+        Console.WriteLine("Введенный вами день недели - выходной!");
+    }
+if (daynumber > 0 && daynumber < 6)
+{
+    Console.WriteLine("Это рабочий день.");
+}
+if (daynumber > 7)
+{
+    Console.WriteLine("Вы ввели не правильную цифру!");
+}
