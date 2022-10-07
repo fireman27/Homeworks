df0c6df1c324ce6bfcc1382cdb42da190ae1d134
fireman <fireman2747@yandex.ru>
2022-10-07 23:41:30 +0800

    Homework 013 проверить оба числа

---
+++ Homework013/Program.cs
@@ -0,0 +1,34 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine() ?? "0"); 
+Console.Write("Введите число: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+if (a == b*b)
+{
+    Console.Write("Да, число ");
+    Console.Write(a);
+    Console.Write(" является квадратом числа ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("Нет, число ");
+    Console.Write(a);
+    Console.Write(" не является квадратом числа ");
+    Console.WriteLine(b);
+}
+if (b == a*a)
+{
+    Console.Write("Да, число ");
+    Console.Write(b);
+    Console.Write(" является квадратом числа ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("Нет, число ");
+    Console.Write(b);
+    Console.Write(" не является квадратом числа ");
+    Console.WriteLine(a);
+}
