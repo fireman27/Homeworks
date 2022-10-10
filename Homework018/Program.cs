d3e709ed5db054f6ba6b8f9e4a1670533723ca9b
fireman <fireman2747@yandex.ru>
2022-10-10 14:23:29 +0800

    Homework 018  найти сумму цифр в числе

---
+++ Homework018/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+while (N != 0)
+{
+    sum += N % 10;
+    N /= 10;
+}
+Console.Write($"Сумма цифр в числе равна - {sum}");
+
