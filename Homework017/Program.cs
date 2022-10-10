19e2413d629510979be47b48b41b7e9de3501d75
fireman <fireman2747@yandex.ru>
2022-10-10 14:03:57 +0800

    Homework 017  найти кубы чисел

---
+++ Homework017/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+Console.Write("Введите число от 1 до N: ");
+double N = double.Parse(Console.ReadLine() ?? "0");
+double a = 1;
+while (a <= N)
+{
+    double b = Math.Pow(a,3);
+    Console.WriteLine($"a = {a}, куб числа a = {b}");
+    a++;
+}
