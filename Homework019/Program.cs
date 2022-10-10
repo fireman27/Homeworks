a771906061a2698ffdf91f48fdaf1eb0e250d848
fireman <fireman2747@yandex.ru>
2022-10-10 14:46:11 +0800

    Homework 019 произведение чисел

---
+++ Homework019/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите число от 1 до N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int a = 1;
+int comp = 1;
+while (a <= N)
+{
+    comp = comp * a;
+    a++;
+}
+Console.Write($"Произведение чисел от 1 до N равна - {comp}");
