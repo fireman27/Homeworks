8ec6265f426eb3ab794cb88e9d3360542157fc7a
fireman <fireman2747@yandex.ru>
2022-10-10 15:07:23 +0800

    Homework 020 кубы чисел четные

---
+++ Homework020/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Введите число от 1 до N: ");
+double N = double.Parse(Console.ReadLine() ?? "0");
+double a = 1;
+while (a <= N)
+{
+    double b = Math.Pow(a,3);
+    a++;
+    if (b % 2 == 0)
+    Console.WriteLine(b);
+}
+Console.WriteLine("Кубы чисел заканчивающиеся на четное число.");
